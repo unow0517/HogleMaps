e245e8f7ab0d36360fd9bd871b7f4b8589352df5
unow0517 <unow1992@gmail.com>
2021-01-05 14:44:16 +0100

    making marker cluster

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ let map;
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.2611809, lng:55.9339575 },
-    zoom: 3,
+    zoom: 3,      
   });
+
+  var icons = {
+    baby: {
+      url: "./image/baby.png",
+      scaledSize: new google.maps.Size(50,50)
+    },
+    education:{
+      url: "./image/education.png",
+      scaledSize: new google.maps.Size(50,50)
+    },
+    internship:{
+      url: "./image/internship.png",
+      scaledSize: new google.maps.Size(50,50)
+    },
+    military:{
+      url: "./image/military.png",
+      scaledSize: new google.maps.Size(50,50)
+    },
+  }
 
   //Busan
   const Busan = { lat:35.23694746417701, lng:129.01179299948885 }
   const markerBusan = new google.maps.Marker({
     position: Busan,
+    icon: icons.baby,
     map: map,
   })
 
@@ -35,6 +55,7 @@ function initMap() {
   const markerSeoul = new google.maps.Marker({
     position: Seoul,
     map: map,
+    icon: icons.education
   })
 
   const SeoulContent = `
@@ -49,6 +70,51 @@ function initMap() {
   markerSeoul.addListener('click',function(){
     infoWindowSeoul.open(map,markerSeoul);
   })
+
+    //internship
+    const SeoulWalk = { lat:37.55426971237671, lng:126.96603377010139 }
+    const markerSeoulWalk = new google.maps.Marker({
+      position: SeoulWalk,
+      map: map,
+      icon: icons.internship
+    })
+
+    const SeoulWalkContent = `
+      <h1>SeoulWalk</h1>
+      <p>my study</p>
+    `
+    
+    const infoWindowSeoulWalk = new google.maps.InfoWindow({
+      content: SeoulWalkContent
+    })
+
+    markerSeoulWalk.addListener('click',function(){
+      infoWindowSeoulWalk.open(map,markerSeoulWalk);
+    })
+    //Internship End
+
+    //internship
+    const DrinkingWater = {lat:37.39174031481825, lng:126.97302689671172 }
+    const markerDrinkingWater = new google.maps.Marker({
+      position: DrinkingWater,
+      map: map,
+      icon: icons.internship
+    })
+
+    const DrinkingWaterContent = `
+      <h1>DrinkingWater</h1>
+      <p>my study</p>
+    `
+    
+    const infoWindowDrinkingWater = new google.maps.InfoWindow({
+      content: DrinkingWaterContent
+    })
+
+    markerDrinkingWater.addListener('click',function(){
+      infoWindowDrinkingWater.open(map,markerDrinkingWater);
+    })
+    //Internship End
+
   //SeoulEnd
 
   //HwaCheon
@@ -56,6 +122,7 @@ function initMap() {
   const markerHwaCheon = new google.maps.Marker({
     position: HwaCheon,
     map: map,
+    icon: icons.military
   })
 
   const HwaCheonContent = `
@@ -77,6 +144,7 @@ function initMap() {
   const markerStuttgart = new google.maps.Marker({
     position: Stuttgart,
     map: map,
+    icon: icons.education
   })
 
   const StuttgarttContent = `
@@ -98,6 +166,7 @@ function initMap() {
   const markerFulda = new google.maps.Marker({
     position: Fulda,
     map: map,
+    icon: icons.education
   })
   
   const FuldatContent = `
@@ -114,6 +183,26 @@ function initMap() {
   })
   //FuldaEnd
   
+  //Add Legend 
   map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend'));
+
+  //Marker cluster
+
+  markers = [
+    markerBusan,
+    markerSeoul,
+    markerHwaCheon,
+    markerDrinkingWater,
+    markerSeoulWalk,
+    markerFulda,
+    markerStuttgart
+  ]
+  new MarkerClusterer(map, markers, 
+    {
+      imagePath:
+      'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+    }
+  );
 }
+
 
