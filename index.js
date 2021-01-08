2295445017ef3792551aea04446106ba3050fe67
unow0517 <unow1992@gmail.com>
2021-01-08 12:48:11 +0100

    put photos, informations on each city

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ let map;
 function initMap() {
   const Busan = { lat:35.23694746417701, lng:129.01179299948885 }
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: Busan,
-    // { lat: 42.2611809, lng:55.9339575 },
-    zoom: 7,      
+    center: { lat: 42.2611809, lng:55.9339575 },
+    zoom: 4,      
   });
 
   var icons = {
@@ -27,6 +26,33 @@ function initMap() {
       url: "./image/military.png",
       scaledSize: new google.maps.Size(50,50)
     },
+    programming:{
+      url: "./image/programming.png",
+      scaledSize: new google.maps.Size(50,50)
+    }
+  }
+
+  var largeIcons = {
+    baby: {
+      url: "./image/baby.png",
+      scaledSize: new google.maps.Size(70,70)
+    },
+    education:{
+      url: "./image/education.png",
+      scaledSize: new google.maps.Size(70,70)
+    },
+    internship:{
+      url: "./image/internship.png",
+      scaledSize: new google.maps.Size(70,70)
+    },
+    military:{
+      url: "./image/military.png",
+      scaledSize: new google.maps.Size(70,70)
+    },
+    programming:{
+      url: "./image/programming.png",
+      scaledSize: new google.maps.Size(70,70)
+    }
   }
 
   //Busan
@@ -41,7 +67,8 @@ function initMap() {
     <div class='citycontent'>
       <h1>Busan</h1>
       <img src="./image/100days.jpg" alt="babyYunHo" width = 200px;>
-      <p>Baby YunHo comes to this world in 1992!</p>
+      <p><b>Baby YunHo comes to this world in 1992!<br>
+      I have lived in this city before I entered Univerisity</b></p>
     </div>
   `
   
@@ -52,81 +79,134 @@ function initMap() {
   markerBusan.addListener('click',function(){
     infoWindowBusan.open(map,markerBusan);
   })
+
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowBusan.close();
+  });
+
+  google.maps.event.addListener(markerBusan, 'mouseover', function(){
+    markerBusan.setIcon(largeIcons.baby)
+  });
+
+  google.maps.event.addListener(markerBusan, 'mouseout', function(){
+    markerBusan.setIcon(icons.baby)
+  });
   //BusanEnd
 
 
-  //Seoul
-  const Seoul = { lat:37.58821968313283, lng:126.9936559964475 }
-  const markerSeoul = new google.maps.Marker({
-    position: Seoul,
+  //Bachelor
+  const Bachelor = { lat:37.58821968313283, lng:126.9936559964475 }
+  const markerBachelor = new google.maps.Marker({
+    position: Bachelor,
     map: map,
     icon: icons.education
   })
 
-  const SeoulContent = `
+  const BachelorContent = `
   <div class='citycontent'>
-    <h1>Seoulwalk</h1>
-    <img src="./image/seoulWalkYunHo.jpg" alt="internshipYunHo" width = 200px;>
-    <p>Baby YunHo comes to this world in 1992!</p>
+    <h1>Bachelor in Landscape Architecture</h1>
+    <img src="./image/uni4.jpg" alt="internshipYunHo" height = 250px;>
+    <img src="./image/uni3.jpg" alt="internshipYunHo" height = 250px;>
+    <p> 2011-2017 (two years leave for military service) </p>
+    <p><b>Bachelor in Landscape Architecture<br>
+    Focused on Landscape Design, Geographic Information System</b></p>
   </div>
 `
   
-  const infoWindowSeoul = new google.maps.InfoWindow({
-    content: SeoulContent
+  const infoWindowBachelor = new google.maps.InfoWindow({
+    content: BachelorContent
   })
 
-  markerSeoul.addListener('click',function(){
-    infoWindowSeoul.open(map,markerSeoul);
+  markerBachelor.addListener('click',function(){
+    infoWindowBachelor.open(map,markerBachelor);
   })
 
-    //internship
-    const SeoulWalk = { lat:37.55426971237671, lng:126.96603377010139 }
-    const markerSeoulWalk = new google.maps.Marker({
-      position: SeoulWalk,
-      map: map,
-      icon: icons.internship
-    })
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowBachelor.close();
+  });
 
-    const SeoulWalkContent = `
-    <div class='citycontent'>
-      <h1>Seoulwalk</h1>
-      <img src="./image/seoulWalkYunHo.jpg" alt="internshipYunHo" width = 200px;>
-      <p>Internship in City Regeneration Social Corporation "SeoulWalk" from 01.2017~ 04.2017</p>
-    </div>
+  google.maps.event.addListener(markerBachelor, 'mouseover', function(){
+    markerBachelor.setIcon(largeIcons.education)
+  });
+
+  google.maps.event.addListener(markerBachelor, 'mouseout', function(){
+    markerBachelor.setIcon(icons.education)
+  });
+  //BachelorEnd
+
+  //Seoulwalk End
+  const SeoulWalk = { lat:37.55426971237671, lng:126.96603377010139 }
+  const markerSeoulWalk = new google.maps.Marker({
+    position: SeoulWalk,
+    map: map,
+    icon: icons.internship
+  })
+  const SeoulWalkContent = `
+  <div class='citycontent'>
+    <h1>Seoulwalk</h1>
+    <img src="./image/seoulWalkYunHo.jpg" alt="internshipYunHo" height = 250px;>
+    <p>01.2017~ 04.2017</p>
+    <p><b>Internship in Social Corporation "SeoulWalk" 
+    Focused on City Regeneration into Pedestrian-centered City </b></p>
+  </div>
   `
-    
-    const infoWindowSeoulWalk = new google.maps.InfoWindow({
-      content: SeoulWalkContent
-    })
 
-    markerSeoulWalk.addListener('click',function(){
-      infoWindowSeoulWalk.open(map,markerSeoulWalk);
-    })
-    //Internship End
+  const infoWindowSeoulWalk = new google.maps.InfoWindow({
+    content: SeoulWalkContent
+  })
+  markerSeoulWalk.addListener('click',function(){
+    infoWindowSeoulWalk.open(map,markerSeoulWalk);
+  })
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowSeoulWalk.close();
+  });
 
-    //internship
-    const DrinkingWater = {lat:37.39174031481825, lng:126.97302689671172 }
-    const markerDrinkingWater = new google.maps.Marker({
-      position: DrinkingWater,
-      map: map,
-      icon: icons.internship
-    })
+  google.maps.event.addListener(markerSeoulWalk, 'mouseover', function(){
+    markerSeoulWalk.setIcon(largeIcons.internship)
+  });
 
-    const DrinkingWaterContent = `
-      <h1>DrinkingWater</h1>
-      <p>my study</p>
-    `
-    
-    const infoWindowDrinkingWater = new google.maps.InfoWindow({
-      content: DrinkingWaterContent
-    })
+  google.maps.event.addListener(markerSeoulWalk, 'mouseout', function(){
+    markerSeoulWalk.setIcon(icons.internship)
+  });
+  //Seoulwalk End
 
-    markerDrinkingWater.addListener('click',function(){
-      infoWindowDrinkingWater.open(map,markerDrinkingWater);
-    })
-    //Internship End
+  //DrinkingWater 
+  const DrinkingWater = {lat:37.39174031481825, lng:126.97302689671172 }
+  const markerDrinkingWater = new google.maps.Marker({
+    position: DrinkingWater,
+    map: map,
+    icon: icons.internship
+  })
+  const DrinkingWaterContent = `
+  <div class='citycontent'>
+    <h1>Help Student in Korea Institute of Drinking Water Research</h1>
+    <img src="./image/intern2.jpg" alt="internshipYunHo" height = 250px;>
+    <img src="./image/intern1.jpg" alt="internshipYunHo" height = 250px;>
+    <img src="./image/intern3.jpg" alt="internshipYunHo" height = 250px;>
+    <p>03.2016~08.2016</p>
+    <p><b>Help Student in Rooftop air-downcooling system with Sedum plants</b></p>
+  </div>
+  `
+  
+  const infoWindowDrinkingWater = new google.maps.InfoWindow({
+    content: DrinkingWaterContent
+  })
+  markerDrinkingWater.addListener('click',function(){
+    infoWindowDrinkingWater.open(map,markerDrinkingWater);
+  })
 
-  //SeoulEnd
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowDrinkingWater.close();
+  });
+
+  google.maps.event.addListener(markerDrinkingWater, 'mouseover', function(){
+    markerDrinkingWater.setIcon(largeIcons.internship)
+  });
+
+  google.maps.event.addListener(markerDrinkingWater, 'mouseout', function(){
+    markerDrinkingWater.setIcon(icons.internship)
+  });
+  //Drinkingwater End
 
   //HwaCheon
   const HwaCheon = { lat:38.199529173356346, lng:127.58830488256483}
@@ -140,8 +220,9 @@ function initMap() {
     <div class='citycontent'>
       <h1>Hwacheon</h1>
       <img src="./image/soldierYunHo.jpeg" alt="soldierYunHo" width =300px;>
-      <p>Mandatory military service for 21months from March.2012 to December.2013<br>
-      it was really close to the border against north Korea, <br>appreciated that I was born in southern part of this land...</p>
+      <p>03.2012 - 12.2013</p>
+      <p><b>Mandatory military service for 21months<br>
+      it was really close to the border against north Korea, <br>appreciated that I was born in southern part of this land...</b></p>
     </div>
   `
   
@@ -152,6 +233,18 @@ function initMap() {
   markerHwaCheon.addListener('click',function(){
     infoWindowHwaCheon.open(map,markerHwaCheon);
   })
+
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowHwaCheon.close();
+  });
+
+  google.maps.event.addListener(markerHwaCheon, 'mouseover', function(){
+    markerHwaCheon.setIcon(largeIcons.military)
+  });
+
+  google.maps.event.addListener(markerHwaCheon, 'mouseout', function(){
+    markerHwaCheon.setIcon(icons.military)
+  });
   //HwaCheonEnd
 
   //Stuttgart
@@ -163,8 +256,18 @@ function initMap() {
   })
 
   const StuttgarttContent = `
+  <div class='citycontent'>
     <h1>Stuttgart</h1>
-    <p>Master in Landscape Ecology</p>
+    <div>
+      <img src="./image/thesis.jpg" alt="thesis" height =250px;>
+      <img src="./image/master2.jpg" alt="library" height =250px;>
+    </div>
+    <img src="./image/master1.jpg" alt="excursion" width =584px;>
+    <p>09.2017 - 09.2019</p>
+    <p><b>Master in Landscape Ecology focused on Geographical Information
+    System</b>
+    </p>
+  </div>
   `
   
   const infoWindowStuttgart = new google.maps.InfoWindow({
@@ -174,6 +277,17 @@ function initMap() {
   markerStuttgart.addListener('click',function(){
     infoWindowStuttgart.open(map,markerStuttgart);
   })
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowStuttgart.close();
+  });
+
+  google.maps.event.addListener(markerStuttgart, 'mouseover', function(){
+    markerStuttgart.setIcon(largeIcons.education)
+  });
+
+  google.maps.event.addListener(markerStuttgart, 'mouseout', function(){
+    markerStuttgart.setIcon(icons.education)
+  });
   //SutttgartEnd
 
   //Fulda
@@ -185,8 +299,14 @@ function initMap() {
   })
   
   const FuldatContent = `
+  <div class='citycontent'>
     <h1>Fulda</h1>
-    <p>Exchange Students in Hochschule Fulda</p>
+    <img src="./image/fulda1.jpg" alt="fuldaImage" height =300px; >
+    <img src="./image/fulda2.jpg" alt="fuldaImgae1" height =300px;>
+    <p>09.2014 - 02.2015</p>
+    <p><b>Exchange Student to Hochschule Fulda during Bachelor</b>
+    </p>
+  </div>
   `
   
   const infoWindowFulda = new google.maps.InfoWindow({
@@ -196,21 +316,74 @@ function initMap() {
   markerFulda.addListener('click',function(){
     infoWindowFulda.open(map,markerFulda);
   })
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowFulda.close();
+  });
+
+  google.maps.event.addListener(markerFulda, 'mouseover', function(){
+    markerFulda.setIcon(largeIcons.education)
+  });
+
+  google.maps.event.addListener(markerFulda, 'mouseout', function(){
+    markerFulda.setIcon(icons.education)
+  });
   //FuldaEnd
+
+  //Geislingen
+  const Geislingen = { lat:48.621691195065374, lng: 9.829502076353009}
+  const markerGeislingen = new google.maps.Marker({
+    position: Geislingen,
+    map: map,
+    icon: icons.programming
+  })
+  
+  const GeislingentContent = `
+  <div class='citycontent'>
+    <h1>Geislingen</h1>
+    <div>
+      <img src="./image/geislingen1.jpg" alt="GeislingenImage" height =300px; >
+      <img src="./image/geislingen2.jpg" alt="GeislingenImgae1" height =300px;>
+    </div>
+    <p>08.2019 - Present</p>
+    <p><b>Learning Web Development by myself finding a secure job </b>
+    </p>
+  </>
+  `
+  
+  const infoWindowGeislingen = new google.maps.InfoWindow({
+    content: GeislingentContent
+  })
+  
+  markerGeislingen.addListener('click',function(){
+    infoWindowGeislingen.open(map,markerGeislingen);
+  })
+  google.maps.event.addListener(map, 'click', function(){
+    infoWindowGeislingen.close();
+  });
+
+  google.maps.event.addListener(markerGeislingen, 'mouseover', function(){
+    markerGeislingen.setIcon(largeIcons.programming)
+  });
+
+  google.maps.event.addListener(markerGeislingen, 'mouseout', function(){
+    markerGeislingen.setIcon(icons.programming)
+  });
+  //GeislingenEnd
   
   //Add Legend 
-  map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend'));
+  const legend = map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend'));
 
   //Marker cluster
 
   markers = [
     markerBusan,
-    markerSeoul,
+    markerBachelor,
     markerHwaCheon,
     markerDrinkingWater,
     markerSeoulWalk,
     markerFulda,
-    markerStuttgart
+    markerStuttgart,
+    markerGeislingen
   ]
   new MarkerClusterer(map, markers, 
     {
@@ -218,6 +391,9 @@ function initMap() {
       'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
     }
   );
+  //marker cluster end
+
+  //click and move
 }
 
 
